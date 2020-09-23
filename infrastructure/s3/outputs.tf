445657d20d0cd200853cd2f16eefb2f416bf72c7
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-09-23 17:58:48 +0100

    added outputs for website hosting buckets

--- infrastructure/s3/outputs.tf
+++ infrastructure/s3/outputs.tf
@@ -1,3 +1,15 @@
 output "website_endpoint" {
   value = aws_s3_bucket.main.website_endpoint
 }
+
+output "website_hosted_zone_id" {
+  value = aws_s3_bucket.main.hosted_zone_id
+}
+
+output "redirect_www_website_endpoint" {
+  value = aws_s3_bucket.redirect_www.website_endpoint
+}
+
+output "redirect_www_website_hosted_zone_id" {
+  value = aws_s3_bucket.redirect_www.hosted_zone_id
+}
