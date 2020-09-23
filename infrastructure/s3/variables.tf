eb01adedf4eed4da58876103c91b4b12108c39f1
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-09-23 13:49:29 +0100

    bucket to host website

---
+++ infrastructure/s3/variables.tf
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "aws_region" {
+  description = "AWS Region where the resources will be created"
+  default     = "eu-west-1"
+}
