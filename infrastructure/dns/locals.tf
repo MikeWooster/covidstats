b34d5f9ad83c747c8d019c983392815c1048f82b
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-09-25 11:03:19 +0100

    add dns config with logging

--- infrastructure/dns/locals.tf
+++ infrastructure/dns/locals.tf
@@ -1,9 +1,9 @@
 
 locals {
-  aws_region = file("../aws_region")
+  aws_region  = file("../aws_region")
   environment = file("../environment")
   application = file("../application")
-  prefix = "${local.application}-${local.environment}"
+  prefix      = "${local.application}-${local.environment}"
   common_tags = {
     Application = local.application
     Environment = local.environment
--- infrastructure/dns/main.tf
+++ infrastructure/dns/main.tf
@@ -39,3 +39,29 @@ resource "aws_route53_record" "covidstats_www_redirect" {
     zone_id = data.terraform_remote_state.s3.outputs.redirect_www_website_hosted_zone_id
   }
 }
+
+# Enable public DNS query logging
+resource "aws_cloudwatch_log_group" "covidstats_public_dns" {
+  name = "/aws/route53/${aws_route53_zone.covidstats.name}"
+  provider = aws.us-east-1
+  retention_in_days = 7
+
+  tags = merge(local.common_tags, {
+    Name = "Public DNS Query Logging"
+  })
+}
+
+# Allow route53 to write to logs.
+resource "aws_cloudwatch_log_resource_policy" "route53_query_logging" {
+  provider = aws.us-east-1
+
+  policy_document = templatefile("templates/route53_query_logging_policy.tmpl", {})
+  policy_name = "route53-query-logging-policy"
+}
+
+resource "aws_route53_query_log" "covidstats_public_dns" {
+  depends_on = [aws_cloudwatch_log_resource_policy.route53_query_logging]
+
+  cloudwatch_log_group_arn = aws_cloudwatch_log_group.covidstats_public_dns.arn
+  zone_id                  = aws_route53_zone.covidstats.zone_id
+}
