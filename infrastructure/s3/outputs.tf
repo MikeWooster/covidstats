94a0eff2cef057022642dc1dfbd785423cddbf19
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-09-25 12:26:24 +0100

    fix routing to buckets

--- infrastructure/s3/outputs.tf
+++ infrastructure/s3/outputs.tf
@@ -1,13 +1,13 @@
-output "website_endpoint" {
-  value = aws_s3_bucket.main.website_endpoint
+output "website_domain" {
+  value = aws_s3_bucket.main.website_domain
 }
 
 output "website_hosted_zone_id" {
   value = aws_s3_bucket.main.hosted_zone_id
 }
 
-output "redirect_www_website_endpoint" {
-  value = aws_s3_bucket.redirect_www.website_endpoint
+output "redirect_www_website_domain" {
+  value = aws_s3_bucket.redirect_www.website_domain
 }
 
 output "redirect_www_website_hosted_zone_id" {
