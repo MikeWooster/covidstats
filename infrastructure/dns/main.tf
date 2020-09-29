e7270b02918829bde7ed2e79d8ecefa1c592c61b
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-09-29 18:09:10 +0100

    issue ssl certificates

--- infrastructure/dns/main.tf
+++ infrastructure/dns/main.tf
@@ -65,3 +65,45 @@ resource "aws_route53_query_log" "covidstats_public_dns" {
   cloudwatch_log_group_arn = aws_cloudwatch_log_group.covidstats_public_dns.arn
   zone_id                  = aws_route53_zone.covidstats.zone_id
 }
+
+#---------------------------------------
+# Set up SSL on the domain
+#---------------------------------------
+
+resource "aws_acm_certificate" "main" {
+  domain_name       = "covidstats.uk"
+  validation_method = "DNS"
+  subject_alternative_names = ["*.covidstats.uk"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create CNAME records supplied by the acm to automatically validate the certificate using DNS
+resource "aws_route53_record" "acm_cert_validation" {
+  depends_on = [aws_acm_certificate.main, aws_route53_zone.covidstats]
+
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.covidstats.zone_id
+}
+
+# Complete the validation
+resource "aws_acm_certificate_validation" "shopaid" {
+  depends_on = [aws_route53_record.acm_cert_validation, aws_acm_certificate.main]
+
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_cert_validation : record.fqdn]
+}
