c44799545b431b664702802ee464f533d1e101cf
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-10-02 16:36:01 +0100

    create new distrobution to redirect www traffic

--- infrastructure/s3/outputs.tf
+++ infrastructure/s3/outputs.tf
@@ -13,3 +13,11 @@ output "website_hosted_zone_id" {
 output "cloudfront_oai_path" {
   value = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
 }
+
+output "redirect_www_website_regional_domain_name" {
+  value = aws_s3_bucket.redirect_www.bucket_regional_domain_name
+}
+
+output "redirect_www_cloudfront_oai_path" {
+  value = aws_cloudfront_origin_access_identity.redirect_www.cloudfront_access_identity_path
+}
