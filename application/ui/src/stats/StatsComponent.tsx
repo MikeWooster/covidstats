c53eebd82c5d3c993e7c4fb5b8d4ad14e7e06640
MikeWooster <mike.wooster@growthstreet.co.uk>
2020-10-12 09:54:25 +0100

    Better mobile experience:
    - move settings into modal
    - better scaling for mobile
    - scale deaths by population

--- application/ui/src/stats/StatsComponent.tsx
+++ application/ui/src/stats/StatsComponent.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Container, Divider, Header, Loader, Segment } from "semantic-ui-react";
+import {
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Loader,
+  Segment,
+} from "semantic-ui-react";
 
 interface props {
   areaOptions: React.ReactElement;
   searchRefinement: React.ReactElement;
   graph: React.ReactElement;
+  settingsModal: React.ReactElement;
   graphOptions: React.ReactElement;
   err: React.ReactElement | null;
   loading: boolean;
@@ -14,25 +22,39 @@ const StatsComponent: React.FC<props> = ({
   areaOptions,
   searchRefinement,
   graph,
+  settingsModal,
   graphOptions,
   err,
   loading,
 }) => {
+  const height =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+
+  // Calculate an approx height for the graph.
+  // header height - render options - "a buffer"
+  const graphHeight = height - 36 - 66 - 50;
+
   return (
     <Container>
       <Header as="h1" textAlign={"center"}>
         Covid Stats UK
       </Header>
       <Segment basic>
-        {areaOptions}
-        {searchRefinement}
-        {err && err}
-        <Loader active={loading} inline />
+        <Grid columns="equal">
+          <Grid.Row>
+            <Grid.Column width={14}>
+              {areaOptions}
+              {searchRefinement}
+              {err && err}
+              <Loader active={loading} inline />
+            </Grid.Column>
+            <Grid.Column width={2}>{settingsModal}</Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Segment>
-      <Divider horizontal />
-      {graph}
-      <Divider horizontal />
-      {graphOptions}
+      <div style={{ height: graphHeight }}>{graph}</div>
     </Container>
   );
 };
